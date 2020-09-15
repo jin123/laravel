d27b70b740b55fe363fbb87871958e448d2a1b5c
jinruimin_v <jinruimin_v@didiglobal.com>
2020-09-15 16:39:21 +0800

    fix

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,12 +5,15 @@ use Illuminate\Routing\Router;
 Admin::routes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
-    'as'            => config('admin.route.prefix') . '.',
-], function (Router $router) {
+    'prefix' => config('admin.route.prefix'),
+    'namespace' => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
+    'as' => config('admin.route.prefix') . '.',
+        ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
-
+    $router->get('/mockBrand/index', 'MockBrandController@index')->name('admin.mockBrand.index');
+    $router->get('/mockBrand/editBrand', 'MockBrandController@editBrand')->name('admin.mockBrand.editBrand');
+    #$router->get('/mockBrand/index', 'MockBrandController@index')->name('brank_lists');
+    $router->get('/mock/index', 'MockController@index')->name('admin.mock.index');
 });
